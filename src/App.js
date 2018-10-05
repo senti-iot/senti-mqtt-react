5553a9f78055fa2c37a5daab8922c290fde32ef3
Christian Broberg <cb@webhouse.dk>
2018-10-05 02:51:17 +0200

    Save

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+// import { options } from './options'
+// var mqtt = require('mqtt')
+import mqtt from 'mqtt'
+
+var client = mqtt.connect('ws://hive.senti.cloud:8083')
+
+// var clientId = options.clientId
+
+/* client.on('connect', function () {
+	client.subscribe('sensor/test', function (err) {
+		if (!err) {			
+			client.publish('sensor/status/' + clientId, 'online ', { retain: false })
+			client.publish('sensor/test', clientId + ': connected')
+			client.subscribe('sensor/update')
+			console.log('Connected')
+		}
+	})
+}) */
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			message: 'State constructed'
+		}
+	}
+
+	componentDidMount() {
+		client.subscribe('sensor/test')
+		client.on('message', (topic, message) => {
+			if (topic.toString() === 'sensor/test') {
+				// console.log(message.toString())
+				this.updateData(message.toString())
+			}
+		})
+	}
+
+	updateData = (received) => {
+		this.setState({ message: received })
+	}
+
+	render() {
+		return (
+			<div>
+				Hello {this.state.message}
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/options.js
@@ -0,0 +1,22 @@
+
+const options = {
+	host: 'ws://hive.senti.cloud',
+	port: '8083',
+	username: '',
+	password: '',
+	keepalive: 60, // 60
+	clientId: 'senti-mqtt-react',
+	clean: true, // false for persistende sessions
+	will: {
+		topic: 'sensor/status/senti-mqtt-react',
+		payload: 'offline (dead)',
+		qos: 1,
+		retain: true
+	},
+	slackChannel: 'https://hooks.slack.com/services/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',
+	logLocale: 'da'
+}
+
+module.exports = {
+	options: options
+}
